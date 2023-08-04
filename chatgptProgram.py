9821bd707dd198c00ddb2eb505a85c5e1e287e8b
Steve Kirsch <stk@m10.io>
2023-08-04 10:39:41 -0700

    updated

--- chatgptProgram.py
+++ chatgptProgram.py
@@ -12,12 +12,16 @@ def probability_at_least_k_red(k, n, N_red, N_blue):
         probability += (comb(n, i) * comb(N_red, i) * comb(N_blue, n - i)) / comb(N_red + N_blue, n)
     return probability
 
-# Given information
-N_red_balls = 31
-N_blue_balls = 7500
-total_draws = 15
+def draw(N_red_draw):
 
-# Calculate probability of at least 3 red balls
-probability_at_least_3_red = probability_at_least_k_red(3, total_draws, N_red_balls, N_blue_balls)
+    # Given information
+    N_red_balls = 31
+    N_blue_balls = 7500
+    total_draws = 15
 
-print(f"The probability of drawing at least 3 red balls out of 15 draws is: {probability_at_least_3_red:.6f}")
+    # Calculate probability of at least 3 red balls
+    probability_at_least_n_red = probability_at_least_k_red(N_red_draw, total_draws, N_red_balls, N_blue_balls)
+
+    print(f"The probability of drawing at least {N_red_draw} red balls out of {total_draws} draws is: {probability_at_least_n_red:.6f}")
+
+draw(3)
