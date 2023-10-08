0c7b7aadc14f28078d369a22308f31233c868399
Steve Kirsch <stk@alum.mit.edu>
2023-10-08 13:05:16 -0700

    new analyze1 defined to make input easier

--- fischer.py
+++ fischer.py
@@ -20,6 +20,10 @@ def analyze(a,b,c,d, description):
     print("Odds ratio=", res.statistic)
     print(res.confidence_interval(confidence_level=0.95))  # 95% confidence interval
 
+# analyze1 will take total cases as the first two arguments
+def analyze1(a,b,c,d,desc):
+    analyze(a-c, b-d, c, d, desc)
+
 analyze(50,143,0,7, "wayne root deaths")
 analyze(50,117,0,33, "wayne root injuries")
 analyze(50, 750, 0, 250, "podiatrist")
@@ -31,18 +35,12 @@ analyze(999999, 140000,1, 15, "jay bonnar deaths per dose vs. FDA claims assumin
 analyze(999999, 14000,1, 4, "jay bonnar same-day deaths per dose vs. FDA claims")
 analyze(999999, 100,1, 3, "my genesis story: 3 relatives who died post jab")
 
-# ths is from the nursing home dataset comparing
-# the week ending 12/6/20 vs. week ending 2/28/21 numbers
-# analyze(511163, 90467, 21952, 6117, "IFR")
-
-# 12/6 vs 2/28
-# 12/6 4808 deaths on 26525 cases control
-# 2/28 621 deaths on 2077 cases
-analyze(26525-4808, 2077-621, 4808, 621, "Nursing home 12/6 vs. 2/28 with one week offset")
-
-
-
 # VHA rates
 # boosted 238 infected; 4 deaths
 # unboosted 739 infected 10 deaths
 analyze(729, 234, 10, 4, "VHA boosted/unboosted death from COVID")
+
+# US Nursing home data before/after rollout
+# Note this is so large that it won't compute a probab
+analyze1(188203, 204890, 31150, 35590, "Nursing home 12 week case")
+analyze1(18820, 20489, 3115, 3559, "Nursing home 12 week case") # faster to compute confidence
