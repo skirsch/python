7ee133c31a9fa5e813bc1a5090a6c788a1330e64
skirsch <stk@alum.mit.edu>
2024-09-14 23:10:05 -0700

    updated normal curve plot

--- normal_distribution_plot.py
+++ normal_distribution_plot.py
@@ -10,7 +10,8 @@ data = np.random.normal(loc=0, scale=1, size=N)
 
 # Define the new bucket edges with two additional buckets on each tail
 # basically, for 3 or more std deviations, there are few data points
-buckets = [-np.inf, -4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, np.inf]
+# buckets = [-np.inf, -4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, np.inf]
+buckets = [-np.inf, -4.25, -3.75, -3.25, -2.75, -2.25, -1.75, -1.25, -0.75, -0.25, 0.25, 0.75, 1.25, 1.75, 2.25, 2.75, 3.25, 3.75, 4.25, np.inf]
 
 # Plot the histogram
 plt.hist(data, bins=buckets, edgecolor='black', rwidth=0.8)
