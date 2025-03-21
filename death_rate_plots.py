bbb7fb3bfbca3f3d083c24b7b6e467dc5d08882c
skirsch <stk@alum.mit.edu>
2025-03-21 14:40:29 -0700

    death rate works

--- death_rate_plots.py
+++ death_rate_plots.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Mortality rates by age for US males starting at age 70
+# Extended mortality rates by age for US males starting at age 50
 data = {
+    50: 0.004890, 51: 0.005321, 52: 0.005810, 53: 0.006363, 54: 0.006973,
+    55: 0.007629, 56: 0.008322, 57: 0.009049, 58: 0.009806, 59: 0.010595,
+    60: 0.011452, 61: 0.012358, 62: 0.013255, 63: 0.014126, 64: 0.015006,
+    65: 0.016001, 66: 0.017124, 67: 0.018298, 68: 0.019519, 69: 0.020847,
     70: 0.022381, 71: 0.024185, 72: 0.026266, 73: 0.028660, 74: 0.031401,
     75: 0.034618, 76: 0.038263, 77: 0.042190, 78: 0.046367, 79: 0.050948,
     80: 0.056237, 81: 0.062360, 82: 0.069226, 83: 0.076884, 84: 0.085452,
@@ -15,38 +19,37 @@ data = {
 # Parameters
 initial_population = 10000
 weeks_per_year = 52
+start_age = 70
+end_age = 72
 
-# Prepare weekly simulation over 30 years
-total_weeks = 30 * weeks_per_year
+# Prepare weekly simulation from start_age to end_age
+total_weeks = int((end_age - start_age) * weeks_per_year)
 weekly_deaths = []
 weekly_mortality_rates = []
 population = initial_population
 
 # Generate age list for each week
-ages = [70 + (w / weeks_per_year) for w in range(total_weeks)]
+ages = [start_age + (w / weeks_per_year) for w in range(total_weeks)]
 
 for week in range(total_weeks):
-    age = 70 + week / weeks_per_year
+    age = start_age + week / weeks_per_year
     age_floor = int(np.floor(age))
     age_ceil = min(age_floor + 1, 100)
-
+    
     # Linear interpolation of mortality rate between integer ages
     weight = age - age_floor
     if age_floor >= 100:
         mortality_rate_annual = data[100]
     else:
         mortality_rate_annual = (1 - weight) * data[age_floor] + weight * data[age_ceil]
-
-    # Convert to weekly rate
-    weekly_mortality = 1 - (1 - mortality_rate_annual) ** (1 / weeks_per_year)
+    
+    # Convert to weekly rate correctly
+    weekly_mortality = 1 - np.exp(np.log(1 - mortality_rate_annual) / weeks_per_year)
     deaths = population * weekly_mortality
 
     weekly_deaths.append(deaths)
-    weekly_mortality_rates.append(weekly_mortality)
+    weekly_mortality_rates.append(mortality_rate_annual * 100)  # Use exact annualized mortality
     population -= deaths
-
-# Convert weekly mortality rates to annualized for plotting
-annualized_mortality_rates = [rate * weeks_per_year for rate in weekly_mortality_rates]
 
 # Plotting
 weeks = np.arange(total_weeks)
@@ -56,13 +59,13 @@ plt.subplot(1, 2, 1)
 plt.plot(weeks, weekly_deaths, label='Weekly Deaths', color='red')
 plt.xlabel('Week')
 plt.ylabel('Number of Deaths')
-plt.title('Weekly Deaths in a Cohort of 10,000 Males (Age 70+)')
+plt.title(f'Weekly Deaths in a Cohort of 10,000 Males (Age {start_age}+)')
 plt.grid(True)
 
 plt.subplot(1, 2, 2)
-plt.plot(weeks, annualized_mortality_rates, label='Annualized Mortality Rate', color='blue')
+plt.plot(weeks, weekly_mortality_rates, label='Annualized Mortality Rate (%)', color='blue')
 plt.xlabel('Week')
-plt.ylabel('Annualized Mortality Rate')
+plt.ylabel('Annualized Mortality Rate (%)')
 plt.title('Annualized Mortality Rate Over Time')
 plt.grid(True)
 
